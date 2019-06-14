ddefed6b73c556292bf42247aba9942618bc6180
DucHoang <ndhdldt1@gmail.com>
2019-06-14 23:21:55 +0700

    Update models/Customer.js

--- models/Customer.js
+++ models/Customer.js
@@ -2,6 +2,7 @@ var mongoose        = require('mongoose'),
     uniqueValidator = require('mongoose-unique-validator');
 
 var CustomersSchema = new mongoose.Schema({
+    phone: {type: Number, required: true, unique: true, trim: true, min: 100000000, max: 999999999},
     name: {type: String, default: ""},
     address: {
         full: {type: String, default: ""},
@@ -9,7 +10,6 @@ var CustomersSchema = new mongoose.Schema({
         district: {type: String, default: ""},
         province: {type: String, default: ""}
     },
-    phone: {type: Number, required: true, unique: true, trim: true, min: 100000000, max: 999999999},
     gender: {type: Number, min: 1, max: 3, default: 3, trim: true},
     birthday: {type: Date, default: null, trim: true},
     id_facebook: {type: Number, default: 0, trim: true},
